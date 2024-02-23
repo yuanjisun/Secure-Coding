d68c348e65259594bde5fb494f8c7666499894f3
Yuanji Sun <31611585+yuanjisun@users.noreply.github.com>
2024-02-23 00:17:05 -0800

    Update Week_5_Lab_Original.c

--- Week_5_Lab_Original.c
+++ Week_5_Lab_Original.c
@@ -6,6 +6,6 @@
 // This program prints the size of a specified file in bytes
 int main(int argc, char** argv) {
 char cmd[BUFSIZE] = "wc -c < ";
-strncpy(cmd, argv[1]);
+strcat(cmd, argv[1]);
 system(cmd);
 }
